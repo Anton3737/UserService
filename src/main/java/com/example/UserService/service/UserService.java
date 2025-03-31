90b4a089f5d620166760178ebea8d9c0b8eadc1d
Anton <BarashASUA@gmail.com>
2025-03-31 14:38:23 +0300

    Initial commit

--- src/main/java/com/example/UserService/service/UserService.java
+++ src/main/java/com/example/UserService/service/UserService.java
@@ -3,12 +3,14 @@ package com.example.UserService.service;
 import com.example.UserService.model.UserEntity;
 import com.example.UserService.repository.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.io.Serializable;
 import java.util.List;
 import java.util.Optional;
 
 @Service
-public class UserService {
+public class UserService implements Serializable {
 
     private final UserRepository userRepository;
 
@@ -17,19 +19,22 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-
+    @Transactional
     public UserEntity createUser(UserEntity user) {
         return userRepository.save(user);
     }
 
+    @Transactional(readOnly = true)
     public List<UserEntity> getAllUsers() {
         return userRepository.findAll();
     }
 
+    @Transactional(readOnly = true)
     public Optional<UserEntity> getUserById(Long id) {
         return userRepository.findById(id);
     }
 
+    @Transactional
     public UserEntity updateUser(Long id, UserEntity updatedUser) {
         return userRepository.findById(id).map(user -> {
             user.setName(updatedUser.getName());
@@ -38,6 +43,7 @@ public class UserService {
         }).orElse(null);
     }
 
+    @Transactional
     public void deleteUser(Long id) {
         userRepository.deleteById(id);
     }
