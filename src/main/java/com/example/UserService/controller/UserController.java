9b0a751fb4d21415ded284668abb70286767daab
Anton <BarashASUA@gmail.com>
2025-06-08 00:17:00 +0300

    Initial commit

--- src/main/java/com/example/UserService/controller/UserController.java
+++ src/main/java/com/example/UserService/controller/UserController.java
@@ -2,17 +2,26 @@ package com.example.UserService.controller;
 
 import com.example.UserService.model.UserEntity;
 import com.example.UserService.service.UserService;
+import jakarta.persistence.EntityExistsException;
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
+//@Slf4j
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
+
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
 
     public UserController(UserService userService) {
         this.userService = userService;
@@ -20,18 +29,22 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<UserEntity> createUser(@RequestBody UserEntity user) {
+        log.info("Create User {}", user);
         return ResponseEntity.ok(userService.createUser(user));
     }
 
+
+
     @GetMapping
     public ResponseEntity<List<UserEntity>> getAllUsers() {
-
+        log.info("Get All Users");
         return ResponseEntity.ok(userService.getAllUsers());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<UserEntity> getUserById(@PathVariable Long id) {
         Optional<UserEntity> user = userService.getUserById(id);
+        log.info("Get User {}", user);
         return user.map(ResponseEntity::ok).orElseGet(() ->
                 ResponseEntity.notFound().build());
     }
@@ -39,6 +52,7 @@ public class UserController {
     @PutMapping("/{id}")
     public ResponseEntity<UserEntity> updateUser(@PathVariable Long id, @RequestBody UserEntity user) {
         UserEntity updatedUser = userService.updateUser(id, user);
+        log.info("Update User {}", user);
         return updatedUser != null ? ResponseEntity.ok(updatedUser) :
                 ResponseEntity.notFound().build();
     }
@@ -46,6 +60,7 @@ public class UserController {
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
+        log.info("Delete User with Id {}", id);
         return ResponseEntity.noContent().build();
     }
 
--- src/main/java/com/example/UserService/security/SecurityConfig.java
+++ src/main/java/com/example/UserService/security/SecurityConfig.java
@@ -1,16 +1,16 @@
 package com.example.UserService.security;
 import com.example.UserService.filter.JwtAuthenticationFilter;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.web.SecurityFilterChain;
-
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
@@ -18,19 +18,47 @@ public class SecurityConfig {
     public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
     }
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws
+//            Exception {
+//        http.csrf(AbstractHttpConfigurer::disable)
+//
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll()
+//                        .requestMatchers("/actuator/prometheus").permitAll()
+//                        .requestMatchers("/auth/**").permitAll()
+//                        .anyRequest().authenticated()
+//                ).addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+//
+//        return http.build();
+//    }
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws
-            Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll()
-                        .requestMatchers("/actuator/prometheus").permitAll()
-                        .requestMatchers("/auth/**").permitAll()
-                        .anyRequest().authenticated()
-                ).addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .csrf().disable()
+//                .authorizeRequests()
+//                .anyRequest().permitAll()
+//                .and()
+//                .httpBasic().disable()
+//                .formLogin().disable()
+//                .logout().disable()
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//
+//        return http.build();
+//    }
 
-        return http.build();
-    }
+
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .csrf().disable()
+//                .authorizeHttpRequests(auth -> auth
+//                        .anyRequest().permitAll()
+//                );
+//        return http.build();
+//    }
+//
+
 
 }
